caad31f1aab7752e2e208dcdbf9ea59f051f0014
Matthias Dillen <matthias.dillen@inuits.eu>
2021-09-08 09:37:24 +0200

    Fix exception handling with failed jobs

--- service/IiifManifest.py
+++ service/IiifManifest.py
@@ -29,44 +29,51 @@ class IiifManifest:
         self.prezi_base_url = prezi_base_url
 
     def generate_manifest(self, entity_id):
-        user = g.oidc_token_info["email"] if hasattr(g, "oidc_token_info") else "default_uploader"
         parent_job = job_helper.create_new_job(job_type="generate_manifest", job_info="Generate manifest parent job")
-        parent_job = job_helper.progress_job(parent_job)
-        fac = ManifestFactory()
+        try:
 
-        fac.set_iiif_image_info(2.0, 2)
-        # Where the resources live on the web
-        fac.set_base_prezi_uri(self.prezi_base_url)
+            fac = ManifestFactory()
 
-        entity = json.loads(
-            requests.get(self.collection_api_base_url + "/entities/" + entity_id).text
-        )
-        mediafiles = json.loads(
-            requests.get(
-                self.collection_api_base_url + "/entities/" + entity_id + "/mediafiles"
-            ).text
-        )
+            fac.set_iiif_image_info(2.0, 2)
+            # Where the resources live on the web
+            fac.set_base_prezi_uri(self.prezi_base_url)
 
-        fac.set_base_image_uri(self.iiif_base_url + "/iiif/2/")
-        entity_metadata = entity["metadata"]
-        lang, title = get_value_from_key_in_dict("title", entity_metadata, True)
-        manifest = fac.manifest(label={lang: title})
-        description = get_value_from_key_in_dict("description", entity_metadata)
-        manifest.set_description(description)
-        seq = manifest.sequence()
-        for mediafile in mediafiles:
-            job = job_helper.create_new_job(job_type="generate_manifest", job_info="Generate manifest")
-            job = job_helper.progress_job(job, parent_job_id=parent_job["_id"])
-            try:
-                id = mediafile["original_file_location"].rsplit("/", 1)[1]
-                job = job_helper.progress_job(job, mediafile_id=id)
-            except:
-                job_helper.fail_job(job, "Missing mediafile id")
-            try:
-                cvs = seq.canvas(ident=id, label=mediafile["filename"])
-                cvs.set_image_annotation(id, iiif=True)
-                job_helper.finish_job(job)
-            except:
-                job_helper.fail_job(job, sys.exc_info()[0])
-        job_helper.finish_job(parent_job)
-        return manifest.toJSON()
+            entity = json.loads(
+                requests.get(self.collection_api_base_url + "/entities/" + entity_id).text
+            )
+            mediafiles = json.loads(
+                requests.get(
+                    self.collection_api_base_url + "/entities/" + entity_id + "/mediafiles"
+                ).text
+            )
+            parent_job = job_helper.progress_job(parent_job, amount_of_jobs=len(mediafiles))
+            fac.set_base_image_uri(self.iiif_base_url + "/iiif/2/")
+            entity_metadata = entity["metadata"]
+            lang, title = get_value_from_key_in_dict("title", entity_metadata, True)
+            manifest = fac.manifest(label={lang: title})
+            description = get_value_from_key_in_dict("description", entity_metadata)
+            manifest.set_description(description)
+            seq = manifest.sequence()
+            for mediafile in mediafiles:
+                job = job_helper.create_new_job(job_type="generate_manifest", job_info="Generate manifest")
+                job = job_helper.progress_job(job, parent_job_id=parent_job["_id"])
+                try:
+                    id = mediafile["original_file_location"].rsplit("/", 1)[1]
+                    job = job_helper.progress_job(job, mediafile_id=id)
+                except Exception as ex:
+                    job_helper.fail_job(job, "Missing mediafile id")
+                    job_helper.fail_job(parent_job, str(ex))
+                    raise ex
+                try:
+                    cvs = seq.canvas(ident=id, label=mediafile["filename"])
+                    cvs.set_image_annotation(id, iiif=True)
+                    job_helper.finish_job(job)
+                except Exception as ex:
+                    job_helper.fail_job(job, str(ex))
+                    job_helper.fail_job(parent_job, str(ex))
+                    raise ex
+            job_helper.finish_job(parent_job)
+            return manifest.toJSON()
+        except Exception as ex:
+            job_helper.fail_job(parent_job, str(ex))
+            raise ex
