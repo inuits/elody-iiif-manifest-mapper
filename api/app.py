b89fd0e8b88088afacf8f9b1100cc7cd282176bd
Matthias Dillen <matthias.dillen@inuits.eu>
2022-03-10 18:53:11 +0100

    Update auth

--- api/app.py
+++ api/app.py
@@ -20,17 +20,16 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 require_oauth = MyResourceProtector(
-    os.getenv("STATIC_JWT", False),
-    {},
-    True if os.getenv("REQUIRE_TOKEN", True) == ("True" or "true" or True) else False,
+    os.getenv("REQUIRE_TOKEN", True) == ("True" or "true" or True),
 )
 validator = JWTValidator(
     logger,
-    os.getenv("STATIC_JWT", False),
     os.getenv("STATIC_ISSUER", False),
     os.getenv("STATIC_PUBLIC_KEY", False),
     os.getenv("REALMS", "").split(","),
-    True if os.getenv("REQUIRE_TOKEN", True) == ("True" or "true" or True) else False,
+    os.getenv("ROLE_PERMISSION_FILE", "role_permission.json"),
+    os.getenv("SUPER_ADMIN_ROLE", "role_super_admin"),
+    os.getenv("REMOTE_TOKEN_VALIDATION", False),
 )
 require_oauth.register_token_validator(validator)
 
