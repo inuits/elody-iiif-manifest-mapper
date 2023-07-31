bcd8ae79baa0cfd05203190f3c06e5fdb26b50b7
Siltn Depuydt <siltn@inuits.eu>
2023-07-31 09:55:25 +0200

    Removed duplicate code via base generator

--- api/generator.py
+++ api/generator.py
@@ -3,72 +3,24 @@ import requests
 
 from exceptions import EntityDoesNotExist, NoMediafiles
 from iiif_prezi.factory import ManifestFactory
+from base_generator import BaseGenerator
 
-
-class Singleton(type):
-    _instances = {}
-
-    def __call__(cls, *args, **kwargs):
-        if cls not in cls._instances:
-            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
-        return cls._instances[cls]
-
-
-class ManifestGenerator(metaclass=Singleton):
+class ManifestGenerator(BaseGenerator):
     def __init__(self):
-        self.collection_api_url = os.getenv("COLLECTION_API_URL")
-        self.image_api_url = os.getenv("IMAGE_API_URL")
-        self.image_api_url_ext = os.getenv("IMAGE_API_URL_EXT")
-        self.presentation_api_url = os.getenv("PRESENTATION_API_URL")
-        self.headers = {"Authorization": f'Bearer {os.getenv("STATIC_JWT")}'}
+        BaseGenerator.__init__(self)
 
     def __add_canvas_to_sequence(self, seq, mediafile):
         ident = mediafile.get("transcode_filename", mediafile["filename"])
         cvs = seq.canvas(ident=ident, label=ident)
         image = cvs.set_image_annotation(ident, iiif=True)
-        image.license = self.__get_license_for_mediafile(mediafile)
-        image.attribution = self.__get_attribution_for_mediafile(mediafile)
+        image.license = self._get_license_for_mediafile(mediafile)
+        image.attribution = self._get_attribution_for_mediafile(mediafile)
         image.resource.id = image.resource.id.replace(
             self.image_api_url, self.image_api_url_ext
         )
         image.resource.service.id = image.resource.service.id.replace(
             self.image_api_url, self.image_api_url_ext
         )
-
-    def __get_attribution_for_mediafile(self, mediafile):
-        ret = f'source: {self.__get_item_metadata_value(mediafile, "source")}'
-        if photographer := self.__get_item_metadata_value(mediafile, "photographer"):
-            ret = f"photographer: {photographer}, {ret}"
-        if rights_holder := self.__get_item_metadata_value(mediafile, "copyright"):
-            ret = f"rightsholder: {rights_holder}, {ret}"
-        return ret
-
-    def __get_from_collection_api(self, endpoint, entity=False, mediafiles=False):
-        req = requests.get(f"{self.collection_api_url}{endpoint}", headers=self.headers)
-        if entity and req.status_code == 404:
-            raise EntityDoesNotExist(req.json()["message"])
-        elif mediafiles and not len(req.json()):
-            raise NoMediafiles("You don't have permission to access this resource")
-        return req.json()
-
-    def __get_item_metadata_value(self, item, key, include_lang=False):
-        for entry in [x for x in item["metadata"] if x["key"] == key]:
-            return (entry["lang"], entry["value"]) if include_lang else entry["value"]
-        return None
-
-    def __get_license_for_mediafile(self, mediafile):
-        license_name = self.__get_item_metadata_value(mediafile, "rights")
-        return {
-            "CC BY-NC 4.0": "https://creativecommons.org/licenses/by-nc/4.0/",
-            "CC BY-NC-ND 4.0": "https://creativecommons.org/licenses/by-nc-nd/4.0/",
-            "CC BY-SA 4.0": "https://creativecommons.org/licenses/by-sa/4.0/",
-            "CC0 1.0": "https://creativecommons.org/publicdomain/zero/1.0/",
-            "Copyright Undetermined": "https://rightsstatements.org/page/UND/1.0/",
-            "In Copyright - non-commercial use permitted": "http://rightsstatements.org/vocab/InC-NC/1.0/",
-            "In Copyright - unknown rightsholder": "http://rightsstatements.org/vocab/InC-RUU/1.0/",
-            "In Copyright": "http://rightsstatements.org/vocab/InC/1.0/",
-            "Public Domain Mark 1.0": "https://creativecommons.org/publicdomain/mark/1.0/",
-        }.get(license_name, "https://rightsstatements.org/page/InC/1.0/?language=en")
 
     def __get_manifest_factory(self):
         fac = ManifestFactory()
@@ -78,17 +30,17 @@ class ManifestGenerator(metaclass=Singleton):
         return fac
 
     def generate_manifest(self, entity_id):
-        entity = self.__get_from_collection_api(f"/entities/{entity_id}", entity=True)
-        mediafiles = self.__get_from_collection_api(
+        entity = self._get_from_collection_api(f"/entities/{entity_id}", entity=True)
+        mediafiles = self._get_from_collection_api(
             f"/entities/{entity_id}/mediafiles", mediafiles=True
         )
-        lang, title = self.__get_item_metadata_value(entity, "title", True)
+        lang, title = self._get_item_metadata_value(entity, "title", True)
         fac = self.__get_manifest_factory()
         manifest = fac.manifest(
             ident=f"{self.presentation_api_url}/manifest/{entity_id}",
             label={lang: title},
         )
-        manifest.set_description(self.__get_item_metadata_value(entity, "description"))
+        manifest.set_description(self._get_item_metadata_value(entity, "description"))
         manifest.rendering = {"@id": entity["data"]["@id"]}
         seq = manifest.sequence()
         for mediafile in mediafiles:
--- api/generatorv3.py
+++ api/generatorv3.py
@@ -6,32 +6,21 @@ import json
 
 from exceptions import EntityDoesNotExist, NoMediafiles
 from iiif_prezi3 import Manifest, KeyValueString
+from base_generator import BaseGenerator
 
-class Singleton(type):
-    _instances = {}
-
-    def __call__(cls, *args, **kwargs):
-        if cls not in cls._instances:
-            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
-        return cls._instances[cls]
-
-class ManifestGeneratorv3(metaclass=Singleton):
+class ManifestGeneratorv3(BaseGenerator):
     def __init__(self):
-        self.collection_api_url = os.getenv("COLLECTION_API_URL")
-        self.image_api_url = os.getenv("IMAGE_API_URL")
-        self.image_api_url_ext = os.getenv("IMAGE_API_URL_EXT")
-        self.presentation_api_url = os.getenv("PRESENTATION_API_URL")
-        self.headers = {"Authorization": f'Bearer {os.getenv("STATIC_JWT")}'}
+        BaseGenerator.__init__(self)
 
     def __add_canvas_to_manifest(self, manifest, mediafile):
         id = mediafile.get("transcode_filename", mediafile["filename"])
 
-        source = self.__get_item_metadata_value(mediafile, "source")
+        source = self._get_item_metadata_value(mediafile, "source")
         image_url = self.image_api_url_ext + "/iiif/3/" + id
         canvas = manifest.make_canvas(
             id=self.presentation_api_url + "canvas/" + id + ".json",
             label=id,
-            rights=self.__get_license_for_mediafile(mediafile),
+            rights=self._get_license_for_mediafile(mediafile),
             requiredStatement=KeyValueString(
                 label="Attribution",
                 value=source
@@ -47,47 +36,20 @@ class ManifestGeneratorv3(metaclass=Singleton):
             anno_page_id="https://annotationpageLink?",
             anno_id="https://annotationLink",
             thumbnail={
-                "id": f'{image_url}/full/{mediafile["img_width"]},{mediafile["img_height"]}/0/default.jpg',
+                "id": f'{image_url}/full/max/0/default.jpg',
                 "type": mediafile["mimetype"]
             },
         )
 
-    def __get_from_collection_api(self, endpoint, entity=False, mediafiles=False):
-        req = requests.get(f"{self.collection_api_url}{endpoint}", headers=self.headers)
-        if entity and req.status_code == 404:
-            raise EntityDoesNotExist(req.json()["message"])
-        elif mediafiles and not len(req.json()):
-            raise NoMediafiles("You don't have permission to access this resource")
-        return req.json()
-
-    def __get_item_metadata_value(self, item, key, include_lang=False):
-        for entry in [x for x in item["metadata"] if x["key"] == key]:
-            return (entry["lang"], entry["value"]) if include_lang else entry["value"]
-        return None
-
-    def __get_license_for_mediafile(self, mediafile):
-        license_name = self.__get_item_metadata_value(mediafile, "rights")
-        return {
-            "CC BY-NC 4.0": "https://creativecommons.org/licenses/by-nc/4.0/",
-            "CC BY-NC-ND 4.0": "https://creativecommons.org/licenses/by-nc-nd/4.0/",
-            "CC BY-SA 4.0": "https://creativecommons.org/licenses/by-sa/4.0/",
-            "CC0 1.0": "https://creativecommons.org/publicdomain/zero/1.0/",
-            "Copyright Undetermined": "https://rightsstatements.org/page/UND/1.0/",
-            "In Copyright - non-commercial use permitted": "http://rightsstatements.org/vocab/InC-NC/1.0/",
-            "In Copyright - unknown rightsholder": "http://rightsstatements.org/vocab/InC-RUU/1.0/",
-            "In Copyright": "http://rightsstatements.org/vocab/InC/1.0/",
-            "Public Domain Mark 1.0": "https://creativecommons.org/publicdomain/mark/1.0/",
-        }.get(license_name, "https://rightsstatements.org/page/InC/1.0/?language=en")
-
     def generate_manifest(self, entity_id):
-        entity = self.__get_from_collection_api(f"/entities/{entity_id}", entity=True)
+        entity = self._get_from_collection_api(f"/entities/{entity_id}", entity=True)
 
-        mediafiles = self.__get_from_collection_api(
+        mediafiles = self._get_from_collection_api(
             f"/entities/{entity_id}/mediafiles", mediafiles=True
         )
 
-        lang, title = self.__get_item_metadata_value(entity, "title", True)
-        description = self.__get_item_metadata_value(entity, "description")
+        lang, title = self._get_item_metadata_value(entity, "title", True)
+        description = self._get_item_metadata_value(entity, "description")
 
         manifest = Manifest(
             id=f"{self.presentation_api_url}/manifest/{entity_id}",
