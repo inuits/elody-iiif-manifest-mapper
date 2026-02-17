4c2346923a6faafe837c191379b93d8f58636406
Tim Standaert <tim.standaert@inuits.eu>
2026-02-17 15:20:37 +0100

    chore: rename sentry -> glitchtip

--- api/app.py
+++ api/app.py
@@ -7,12 +7,12 @@ from flask import Flask
 from flask_restful import Api
 from healthcheck import HealthCheck
 
-if os.getenv("SENTRY_ENABLED", False) in ["True", "true", True]:
+if os.getenv("GLITCH_TIP_ENABLED", False) in ["True", "true", True]:
     import sentry_sdk
     from sentry_sdk.integrations.flask import FlaskIntegration
 
     sentry_sdk.init(
-        dsn=os.getenv("SENTRY_DSN"),
+        dsn=os.getenv("GLITCH_TIP_DSN"),
         integrations=[FlaskIntegration()],
         environment=os.getenv("NOMAD_NAMESPACE"),
     )
